429ecf8f488940dcd2ed627156f3f179cda66e6f
Sybonade <axelpuputti23@gmail.com>
2024-03-22 23:36:06 +0200

    added header and footer

---
+++ includes/header.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/styles.css">
+    <title>Document</title>
+</head>
+<body>
+
+<header class="container-fluid ">
+	
+	<div class="hero-section ">
+  <nav class="navbar navbar-expand-lg navbar-dark shadow-5-strong">
+  <div class="container-fluid">
+  
+    <a class="navbar-brand" href="index.php">Tech help</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" href="index.php">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="contact.php">Contact</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="aboutme.php">About me</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<br>
+<br>
+<br>
+<br>
+<br>
+		<div  class="col-sm-6 px-5 ">
+			<h1>Service to your door</h1>
+			<p>If you need help at home with tech problems, here is your solution</p>
+			<div class="btn bg-danger text-white px-3 py-2">Contact now</div>
+		</div>
+		<div class="col-sm-6"></div>
+	</div>
+	</div>
+</header>
