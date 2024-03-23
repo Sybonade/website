04f972daea6c6f89e5924420ba20c00106b3ca8a
Sybonade <axelpuputti23@gmail.com>
2024-03-23 14:52:33 +0200

    made some changes to head and added a main section for index

--- index.php
+++ index.php
@@ -1,7 +1,43 @@
 <?php 
 include 'includes/header.php';
 ?>
-<br>
+
+<div class="container-fluid pt-5 main-section" style="">
+    <h1 class="main-section-header text-center ">This is example
+    </h1>
+
+<div class="row">
+  <div class="col-sm-4 text-center">
+    <h2>This is a example text </h2>
+
+  </div>
+  <div class="col-sm-8">
+    <br>
+    <div class="row">
+    <div class="col mr-5">
+      <h3> This is placeholder </h3>
+      <p > Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+       Pellentesque id dolor lacinia lacus blandit pharetra.
+       Fusce iaculis massa nec lacinia condimentum. Fusce ut condimentum lacus. 
+      Fusce turpis dui, imperdiet lobortis orci vel, imperdiet ornare nunc.
+    </p>
+
+    </div>
+    <div class="col mr-5">
+    <h3> This is placeholder </h3>
+    <p > Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+       Pellentesque id dolor lacinia lacus blandit pharetra.
+       Fusce iaculis massa nec lacinia condimentum. Fusce ut condimentum lacus. 
+      Fusce turpis dui, imperdiet lobortis orci vel, imperdiet ornare nunc.
+    </p>
+
+    </div>
+</div>
+
+  </div>
+  
+</div>
+</div>
 
   <?php 
 include 'includes/footer.php';
